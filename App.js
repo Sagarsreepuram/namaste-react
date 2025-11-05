57d8e1d1a7108c099798a54f7517fbf542cf9cae
Sagarsreepuram <sagarsreepuram06@gmail.com>
2025-11-05 15:41:44 +0530

    Commit

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-const parent = React.createElement("div",{id:"parent"}, [
-    React.createElement("div", {id:"child"},[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div",{id:"child2"}, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
+const heading = React.createElement(
+  "h1",
+  {id: "heading"},
+  "Namesthe React 🚀"
+)
 
-const root = ReactDom.creteRoot(document.getElementById("header"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 # Namaste react
+
+# Parcel 
+Dev Build 
+Local Server
+HMR = Hot Module Replacement 
+File watching Algorithm - Written in c++ 
